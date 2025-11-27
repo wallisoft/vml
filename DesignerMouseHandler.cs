d00100221e2c2f40444ee29092e4dd37c340da1f
wallisoft <wallisoft@gmail.com>
2025-11-27 04:32:25 +0000

    Auto: 2025-11-27 04:32

--- DesignerMouseHandler.cs
+++ DesignerMouseHandler.cs
@@ -57,12 +57,6 @@ public static class DesignerMouseHandler
             
             // Right click - context menu (check for VML menu first)
             if (point.Properties.IsRightButtonPressed)
-                // Store click position for scripts
-                // Store click position for scripts
-                Settings.Set("click_x", canvasPos.X.ToString());
-                Settings.Set("click_y", canvasPos.Y.ToString());
-                Settings.Set("click_x", canvasPos.X.ToString());
-                Settings.Set("click_y", canvasPos.Y.ToString());
             {
                 var hit = designCanvas.InputHitTest(canvasPos);
                 
